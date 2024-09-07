1e501327e23f92caa7b7744047d2c7f71e46aede
bhataasim1 <bhataasim700@gmail.com>
2024-09-07 17:27:20 +0530

    Add AppRoutes component for routing and authentication

---
+++ client/src/AppRoutes.tsx
@@ -0,0 +1,52 @@
+import { Route, Routes } from "react-router-dom";
+import createStore from "react-auth-kit/createStore";
+import AuthProvider from "react-auth-kit/AuthProvider";
+import AuthOutlet from "@auth-kit/react-router/AuthOutlet";
+import RootLayout from "./layout/RootLayout";
+import { Login } from "./pages/Login";
+import { Register } from "./pages/Register";
+import { TaskBoard } from "./components/TaskBoard";
+
+const store = createStore({
+  authName: "voosh_auth_token",
+  authType: "cookie",
+  cookieDomain: window.location.hostname,
+  cookieSecure: window.location.protocol === "https:",
+});
+
+const AppRoutes = () => {
+  return (
+    <AuthProvider store={store}>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <RootLayout>
+              <Login />
+            </RootLayout>
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <RootLayout>
+              <Register />
+            </RootLayout>
+          }
+        />
+        <Route element={<AuthOutlet fallbackPath="/" />}>
+          <Route
+            path="/dashboard"
+            element={
+              <RootLayout>
+                <TaskBoard />
+              </RootLayout>
+            }
+          />
+        </Route>
+      </Routes>
+    </AuthProvider>
+  );
+};
+
+export default AppRoutes;
