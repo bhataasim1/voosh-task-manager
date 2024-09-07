43d526ea3689477f86444dcfbf167514c53a2fba
bhataasim1 <bhataasim700@gmail.com>
2024-09-07 17:27:28 +0530

    Refactor main.tsx to use BrowserRouter for routing and add Toaster component

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import "./index.css";
+import AppRoutes from "./AppRoutes";
+import { Toaster } from "react-hot-toast";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Toaster position="top-right" reverseOrder={false} />
+    <Router>
+      <AppRoutes />
+    </Router>
+  </StrictMode>
+);
