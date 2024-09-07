ec714e924f1a73f6f12243dd09b8e45e78b67ec6
bhataasim1 <bhataasim700@gmail.com>
2024-09-07 13:49:32 +0530

    Add auth and task routes with controllers and middleware

---
+++ server/routes/task.routes.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import { TaskController } from "../controller/task/task.controller";
+import { authMiddleware } from "../middleware/auth.middleware";
+
+const taskRouter = express.Router();
+const taskController = new TaskController();
+
+taskRouter.use(authMiddleware);
+
+taskRouter.get("/", taskController.getAllTasks);
+taskRouter.post("/create-task", taskController.createTask);
+taskRouter.put("/update-task/:id", taskController.updateTask);
+taskRouter.delete("/delete-task/:id", taskController.deleteTask);
+
+export default taskRouter;
