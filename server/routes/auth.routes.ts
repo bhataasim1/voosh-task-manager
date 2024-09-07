2702dd03b7b1f36159140e936c64d8c11a52a9db
bhataasim1 <bhataasim700@gmail.com>
2024-09-07 14:34:53 +0530

    Add Google OAuth routes to auth.routes.ts

--- server/routes/auth.routes.ts
+++ server/routes/auth.routes.ts
@@ -7,4 +7,7 @@ const authController = new AuthController();
 authRouter.post("/register", authController.register);
 authRouter.post("/login", authController.login);
 
+authRouter.get("/google", authController.googleAuth);
+authRouter.get("/google/callback", authController.googleCallback);
+
 export default authRouter;
