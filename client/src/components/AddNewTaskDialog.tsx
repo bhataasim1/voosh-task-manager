0558a81a029b66442771b6095eaf82c22dd8ff72
bhataasim1 <bhataasim700@gmail.com>
2024-09-08 09:10:55 +0530

    Refactor AddNewTaskDialog component to use Dialog component for better user experience

---
+++ client/src/components/AddNewTaskDialog.tsx
@@ -0,0 +1,83 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "./ui/button";
+import { DialogClose } from "@radix-ui/react-dialog";
+import { useState } from "react";
+import { useTask } from "@/hooks/useTask";
+
+const AddNewTaskDialog = () => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const { createTask } = useTask();
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const handleSubmit = async () => {
+    setLoading(true);
+    try {
+      await createTask({ title, description, status: "TODO" });
+      setTitle("");
+      setDescription("");
+    } catch (error) {
+      console.error("Error creating task", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <Dialog>
+      <DialogTrigger>
+        <Button className="text-white bg-blue-500 border rounded-md px-2 py-1 hover:bg-blue-800">
+          Add Task
+        </Button>
+      </DialogTrigger>
+      <form onSubmit={handleSubmit} className="mb-8">
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Add Task</DialogTitle>
+            <DialogDescription>
+              <input
+                type="text"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                placeholder="Task title"
+                className="w-full px-3 py-2 border rounded mb-2"
+                required
+              />
+              <textarea
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+                placeholder="Task description"
+                className="w-full px-3 py-2 border rounded mb-2"
+              />
+            </DialogDescription>
+          </DialogHeader>
+          <DialogFooter>
+            <DialogClose>
+              <Button
+                variant={"outline"}
+                className="m-2"
+                onClick={() => handleSubmit()}
+                disabled={loading}
+              >
+                Add Task
+              </Button>
+              <Button variant={"destructive"} className="m-2">
+                Cancel
+              </Button>
+            </DialogClose>
+          </DialogFooter>
+        </DialogContent>
+      </form>
+    </Dialog>
+  );
+};
+
+export default AddNewTaskDialog;
